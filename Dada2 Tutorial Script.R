ac74a49d78f870eebbf658de3dd6c4015830a50a
Samantha Seibel <sls6550@psu.edu>
2023-02-10 15:42:37 -0500

    Beginning Dada2 tutorial, attempt to visualize fastq plots

---
+++ Dada2 Tutorial Script.R
@@ -0,0 +1,19 @@
+#Dada2 Tutorial
+#Good luck me
+
+#load the dada2 package
+library(dada2); packageVersion("dada2")
+
+#path to directory containing unzipped fastq files
+path <- "/c/Users/saman/OneDrive - The Pennsylvania State University/Ganda Lab/Spring 2023/Dada2_Tutorial/MiSeq_SOP"
+list.files(path)
+
+# Forward and reverse fastq filenames have format: SAMPLENAME_R1_001.fastq and SAMPLENAME_R2_001.fastq
+fnFs <- sort(list.files(path, pattern="_R1_001.fastq", full.names = TRUE))
+fnRs <- sort(list.files(path, pattern="_R2_001.fastq", full.names = TRUE))
+
+# Extract sample names, assuming filenames have format: SAMPLENAME_XXX.fastq
+sample.names <- sapply(strsplit(basename(fnFs), "_"), `[`, 1)
+
+#visualize quality profiles of forward reads
+plotQualityProfile(fnFs[1:2])
